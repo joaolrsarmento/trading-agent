a2e9c333b9d5a5752f8790684b5df9992af9919e
João Sarmento <joaolrsarmento@gmail.com>
2020-06-04 01:57:33 -0300

    created log on tools

--- models/AbstractModel.py
+++ models/AbstractModel.py
@@ -18,42 +18,16 @@ class AbstractModel(object):
         self.model_name = model_name
 
 
-    def run_backtest(self, agent = None, initial_balance=1000.00, initial_date="2010-01-01", final_date="2019-01-01", symbols=["AAPL"]):
+    def run_tool(self, tool):
         """
         Method to run backtest on specific symbols using the same model.
 
-        @param agent: agent that the model is running on.
-        @@type agent: class Agent
-        @param initial_balance: value in the begin.
-        @@type initial_balance: float
-        @param initial_date: initial date to backtest
-        @@type initial_date: string (datetime)
-        @param final_date: final date to backtest
-        @@type final_date: string (datetime)
-        @param symbols: symbols that represents the shares to be tested at
-        @@type symbols: list of strings
-
-        @return history: history of trades
-        @@@type history: list
-        @return final_balance: values in the end
-        @@@type final_balance: list
         """
-
-        # Checking for error
-        if not self.agent:
-            raise NotImplementedError("Agent not implemented.")
-
-        final_balance = np.array([] * len(symbols))
-        history = np.array([] * len(symbols))
-
-        backtest = BacktestTool(symbols)
-        data = backtest.get_data(initial_date, final_date)
-
-        data['Signal'] = self.agent.get_signals(data['Close'])
-
+        tool.execute()
+        
     def get_signals(self, data = None):
         """
-        Abstract.
+        Method to get signals from a model.
 
         @param data: empty
         @@type data: None
