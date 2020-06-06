bfcc42aedb6a54280cd227d144d2ac28a948ac89
João Sarmento <joaolrsarmento@gmail.com>
2020-06-06 19:14:37 -0300

    added plot methods

--- main.py
+++ main.py
@@ -4,4 +4,3 @@ from tools.BacktestTool import BacktestTool
 agent = BasicAgent(balance=1000, take_profit=0.03, stop_loss=0.01)
 backtest = BacktestTool(initial_date="2020-01-01", final_date="2020-06-01")
 agent.run_tool(backtest, save_log=True)
-agent.plot()
