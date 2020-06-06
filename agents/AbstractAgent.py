9be9a2a2a40158f44f8d20f0949af45e8fcfec6f
João Sarmento <joaolrsarmento@gmail.com>
2020-06-06 14:20:34 -0300

    added documentation

--- agents/AbstractAgent.py
+++ agents/AbstractAgent.py
@@ -42,16 +42,6 @@ class AbstractAgent(object):
         # Print
         print(f'Added model {model.get_name()}')
 
-    def _create_operation(self, signals, position):
-
-        operation = Operation(close_price=signals['Close'][-1],
-                              invested_value=self.balance * self.active_balance_percentage,
-                              take_profit=self.take_profit,
-                              stop_loss=self.stop_loss,
-                              position=position)
-
-        self.operations.append(operation)
-
     def update(self, data):
         """
         Updated agent data. It's worth mentioning that we don't check for divergence on this agent.
@@ -107,19 +97,58 @@ class AbstractAgent(object):
         return self.signals
 
     def _should_create_operation(self, signals):
+        """
+        Check if should create a new operation based on last signal.
+
+        @param signals: all the signals until this moment.
+        @@type signals: pandas dataframe
+
+        @return boolean that represents the operation creation
+        """
         if signals['Signal'][-1] in [BUY, SELL]:
             return True
 
         return False
 
     def _update_operations(self, data):
+        """
+        Go through all operation to check if any should be closed. Also, check for operation creation
+
+        @param data: data updated
+        @@type data: pandas dataframe
+        """
         # Check for operation creation
         if self._should_create_operation(self.signals):
+            # Get last position
             position = self.signals['Signal'][-1]
+            # Create
             self._create_operation(self.signals, position)
 
         # Check for operation closing
         for operation in self.operations:
+            # If it is still open
             if operation.is_open_position():
+                # Check for endpoint
                 if operation.reached_endpoint(data['Close'][-1]):
-                    operation.close()
+                    # Store operation data
+                    operation_id, operation_result, operation_profit = operation.close()
+    
+    def _create_operation(self, signals, position):
+        """
+        Creates a new operation. For now, its only theoretical.
+
+        @param signals: updated signals
+        @@type signals: pandas dataframe
+        @param position: position the operation should run on
+        @@type position: BUY or SELL constant
+        """
+
+        # Create operation
+        operation = Operation(id=len(self.operations),
+                              close_price=signals['Close'][-1],
+                              invested_value=self.balance * self.active_balance_percentage,
+                              take_profit=self.take_profit,
+                              stop_loss=self.stop_loss,
+                              position=position)
+        # Save it
+        self.operations.append(operation)
--- models/Operation.py
+++ models/Operation.py
@@ -4,19 +4,49 @@ from utils.constants import BUY, SELL, DO_NOTHING
 
 
 class Operation(object):
-    def __init__(self, close_price, invested_value, take_profit, stop_loss, position):
+    """
+    Class that represents a operation.
+
+    """
+
+    def __init__(self, id, close_price, invested_value, take_profit, stop_loss, position):
+        """
+        Class constructor.
+
+        @param id: id for log purposes.
+        @@type id: integer
+        @param close_price: the close_price the operation went on
+        @@type close_price: float
+        @param invested_value: invested value on the operation
+        @@type invested_value: float
+        @param take_profit: profit stop constant
+        @@type take_profit: float (is percentage)
+        @param stop_loss: loss stop constant
+        @@type stop_loss: float (is percentage)
+        @param position: position the operation should run on
+        @@type position: BUY or SELL
+        """
         self._initial_price = close_price
         self._final_price = close_price
-        self._operation_result = None
-        self._operation_closed = False
+        self._result = None
+        self._closed = False
         self._profit = 0
         self._position = position
+        self._id = id
 
         self.invested_value = invested_value
         self.take_profit = take_profit
         self.stop_loss = stop_loss
 
     def reached_endpoint(self, updated_price):
+        """
+        Check for operation end.
+
+        @param updated_price: updated close price
+        @@type updated_price: float
+
+        @return boolean that represents that if the operation should close
+        """
         if self._position == BUY:
             if updated_price > (1 + self.take_profit) * self._initial_price or updated_price < (1 - self.stop_loss) * self._initial_price:
                 self._final_price = updated_price
@@ -31,24 +61,52 @@ class Operation(object):
                 return False
 
     def close(self):
-        self._operation_closed = True
+        """
+        Close operation.
+
+        @return _id: operation id
+        @@@type _id: integer
+        @return _result: operation result
+        @@@type _result: boolean
+        @return _profit: operation profit
+        @@@type _profit: operation profit
+        """
+        self._closed = True
         self._profit = self.get_profit()
+        self._result = True if (self._final_price - self._initial_price) * self._position > 0 else False
 
-        if self._final_price > self._initial_price:
-            self._operation_result = True
-        else:
-            self._operation_result = False
-
-        return self._profit
+        return self._id, self._result, self._profit
 
     def get_final_price(self):
+        """
+        Get final price.
+
+        @return _final_price: final price
+        """
         return self._final_price
 
     def get_initial_price(self):
+        """
+        Get initial price.
+
+        @return _initial_price: final price
+        """
         return self._initial_price
 
     def get_profit(self):
+        """
+        Calculates operation profit
+        
+        @return: operation profit
+        @@@type: float
+        """
         return self.invested_value * self._position * (self._final_price - self._initial_price) / self._initial_price
 
     def is_open_position(self):
-        return not self._operation_closed
+        """
+        Check if it is still open.
+
+        @return _closed: operation status
+        @@@type _closed: boolean
+        """
+        return not self._closed
